5845feef9407aaae2a2b2f16ed4d9d8e158965c8
Craig Fulton <ckfeleven@gmail.com>
2018-08-23 21:22:10 +0100

    borked the database, rebuilt and start again.

--- flsk_proj.py
+++ flsk_proj.py
@@ -6,8 +6,8 @@ Created on Mon Jul 16 20:49:43 2018
 """
 
 from app import app, db
-from app.models import User, Post, Minicard, Project
+from app.models import User, Post, Minicard, Project, Team
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Minicard': Minicard, 'Project': Project}
+    return {'db': db, 'User': User, 'Post': Post, 'Minicard': Minicard, 'Project': Project, 'Team': Team}
